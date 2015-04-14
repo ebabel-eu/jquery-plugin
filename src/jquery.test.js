643004f16aee8a54936a3938bb5e7d677cfe79b6
Nadjib Amar <hello@ebabel.eu>
2015-04-14 07:16:49 +0200

    add incomplete format for a jQuery plugin.

--- src/jquery.test.js
+++ src/jquery.test.js
@@ -4,5 +4,9 @@
 (function ($) {
     "use strict";
 
+    // Note: this incomplete format for a jQuery plugin would work on the entire selection *at once*, which is not desirable.
+    $.fn.test = function () {
+        // Plugin code
+    }
 
 }(jQuery));
