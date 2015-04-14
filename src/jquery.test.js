ed16428636fa2b7527252e371698251399a9a463
Nadjib Amar <hello@ebabel.eu>
2015-04-14 07:32:16 +0200

    add jquery dependency and a sample html file using the plugin.

--- src/jquery.test.js
+++ src/jquery.test.js
@@ -1,12 +1,15 @@
 /*jslint browser: true */
 /*global jQuery */
+
 // Shell for the plugin code
 (function ($) {
     "use strict";
 
-    // Note: this incomplete format for a jQuery plugin would work on the entire selection *at once*, which is not desirable.
     $.fn.test = function () {
         // Plugin code
-    }
+        return this.each(function () {
+            // Do something to each item.
+        });
+    };
 
 }(jQuery));
