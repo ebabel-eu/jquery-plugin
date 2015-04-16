e4b51b3633a54535a375ca8a2c25cd8f074dd278
Nadjib Amar <hello@ebabel.eu>
2015-04-16 15:34:01 +0200

    add jquery-ui dependency, store data and attach a once off even handler.

--- src/jquery.test.js
+++ src/jquery.test.js
@@ -8,8 +8,19 @@
     $.fn.test = function () {
         // Plugin code
         return this.each(function () {
-            // Do something to each item, for example highlight it in yellow.
-            $(this).css("background-color", "#ff0");
+            // Do something to each item in turn, not all at once (better performance).
+            $(this)
+                // First, store the initial background color as data without changing the HTML.
+                .data("original-color", $(this).css("background-color"))
+                // Secondly, set the current background color to a given color.
+                .css("background-color", "#fff47f")
+                // Finally, attach an event handler that will only run once and then un-attach itself (good for performance).
+                .one("mouseenter", function() {
+                    // Animate to the stored background color.
+                    $(this).animate({
+                        "background-color": $(this).data("original-color")
+                    }, "fast");
+                });
         });
     };
 
