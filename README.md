e6e937ea11a1391971a3f942983848ceb6eb2212
Nadjib Amar <hello@ebabel.eu>
2015-04-17 07:41:53 +0200

    add options

--- README.md
+++ README.md
@@ -3,25 +3,64 @@ Sample jQuery plugin to test how to write one with typical features.
 
 ## Install jQuery test plugin
 The bower command will install both the plugin and its dependencies.
+
 ```
 install bower https://github.com/ebabel-eu/jquery-plugin.git --save
 ```
 
 ## Use jQuery test plugin
+
 ```
 $("selector").test();
 ```
 
 Example:
+
 ```
 $("p").test();
 ```
 
 See the [sample usage](src/index.html) in HTML.
 
+## Options
+You can configure the plugin options if you don't want to use its default settings.
+
+It's possible to set one or several options at once.
+
+Example:
+
+```
+$("p").test({
+    colour: "#ff69b4",
+    duration: "slow"
+});
+```
+
+### colour
+The colour option sets the colour of the highlighting and can be any valid CSS colour.
+
+Example:
+
+```
+$("p").test({
+    colour: "#ff69b4"
+});
+```
+
+### duration
+The duration sets how fast the animation from highlighted colour to original colour should be. The value can be either a number or a string (slow, fast).
+
+Example:
+
+```
+$("p").test({
+    duration: "slow"
+});
+```
+
+
 ## todo:
 
-* Add configuration of options.
 * Add Grunt for src build and dist build..
 * Add ESLint.
 * Add HTML linting for the sample file.
--- src/jquery.test.js
+++ src/jquery.test.js
@@ -5,7 +5,10 @@
 (function ($) {
     "use strict";
 
-    $.fn.test = function () {
+    $.fn.test = function (options) {
+        // Allow options and use defaults wherever an option isn't specified.
+        options = $.extend($.fn.test.defaults, options);
+
         // Plugin code
         return this.each(function () {
             // Do something to each item in turn, not all at once (better performance).
@@ -13,15 +16,20 @@
                 // First, store the initial background color as data without changing the HTML.
                 .data("original-color", $(this).css("background-color"))
                 // Secondly, set the current background color to a given color.
-                .css("background-color", "#fff47f")
+                .css("background-color", options.colour)
                 // Finally, attach an event handler that will only run once and then un-attach itself (good for performance).
-                .one("mouseenter", function() {
+                .one("mouseenter", function () {
                     // Animate to the stored background color.
                     $(this).animate({
                         "background-color": $(this).data("original-color")
-                    }, "fast");
+                    }, options.duration);
                 });
         });
+    };
+
+    $.fn.test.defaults = {
+        colour: "#fff47f",
+        duration: "fast"
     };
 
 }(jQuery));
