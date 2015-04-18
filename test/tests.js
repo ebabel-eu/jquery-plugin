6cf405ac0b9cf7a1f2a258cbaf06f133bf2feb1b
Nadjib Amar <hello@ebabel.eu>
2015-04-18 23:11:18 +0200

    add different types of tests.

--- test/tests.js
+++ test/tests.js
@@ -1,14 +1,49 @@
-// todo: write lots of unit tests...
 // todo: can Istanbul work with QUnit?
 
 (function ($, QUnit) {
 
+    QUnit.module( "Plugin applied to a div", {
+        beforeEach: function () {
+            $("body").append("<div id=\"lorem\">lorem ipsum</div>");
+        },
+        afterEach: function () {
+            $("#lorem").remove();
+        }
+    });
+
+    // Test if something is defined.
     QUnit.test("plugin exists", function (assert) {
-        assert.ok( $("p").test, "Passed!" );
+        assert.ok($("p").test);
     });
 
+    // Test if something is a function
     QUnit.test("plugin is a function", function (assert) {
-        assert.ok( $.isFunction($("p").test), "Passed!" );
+        assert.ok($.isFunction($("p").test));
     });
 
-}(jQuery, QUnit));
+    // Equality test
+    QUnit.test("change the background colour to red", function (assert) {
+        $("#lorem").test({
+            colour: "#f00"
+        });
+        assert.equal($("#lorem").css("background-color"), "rgb(255, 0, 0)");
+    });
+
+    // Asynchronous test
+    QUnit.test("rollover changes the element background colour", function (assert) {
+        var done = assert.async();
+
+        $("#lorem").test({
+            colour: "#ff0",
+            duration: 1
+        });
+
+        $("#lorem").trigger("mouseenter");
+
+        setTimeout(function() {
+            assert.notEqual($("#lorem").css("background-color"), "rgb(255, 255, 0)");
+            done();
+        }, 15);
+    });
+
+}(window.jQuery, window.QUnit));
